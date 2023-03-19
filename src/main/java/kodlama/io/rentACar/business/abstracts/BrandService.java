37121ae5aedca8856df3589e83b1ebfd3fe00405
ceylancem <cemceylann@icloud.com>
2023-03-19 22:01:30 +0300

    COMMIT

---
+++ src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -0,0 +1,22 @@
+package kodlama.io.rentACar.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandReponse;
+
+public interface BrandService {
+
+	List<GetAllBrandsResponse> getAll();
+
+	GetByIdBrandReponse getById(int id);
+
+	void add(CreateBrandRequest createBrandRequest);
+
+	void update(UpdateBrandRequest updateBrandRequest);
+
+	void delete(int id);
+
+}
---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,60 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "brands")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Brand {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@OneToMany(mappedBy = "brand")
+	private List<Model> models;
+
+//	public Brand() {
+//
+//	}
+//
+//	public Brand(int id, String name) {
+//		this.id = id;
+//		this.name = name;
+//	}
+
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+
+}
